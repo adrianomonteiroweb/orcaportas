2f5ed07e2501770764f18d0b6b1225e0778582ba
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-06-27 15:40:53 -0300

    new: budget form implemented like component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './src/components/HomeScreen';
+import BudgetFormScreen from './src/components/BudgetFormScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='BudgetForm' component={BudgetFormScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/components/BudgetFormScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const BudgetFormScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Formulário de Orçamento de Portas</Text>
+      {/* Adicione seus componentes de formulário aqui */}
+    </View>
+  );
+};
+
+export default BudgetFormScreen;
